1d95cef61a7c1d953d87df36d35d367e7ae6b0f3
Mosaab Emam <mosaab.emam123@gmail.com>
2023-01-13 17:03:32 +0000

    :poop:

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,82 @@
-// generate use statements for the code below
-use regex::Regex;
-use std::fs::{self, DirEntry, ReadDir};
+use stackcheck::{has_file, PackageManager, UsesComposerPackageManager};
+extern crate glob;
+use glob::glob;
+use std::fmt::Display;
+use std::fs::{self, ReadDir};
 use std::path::Path;
+
+fn detect_package_manager(path: &str) -> Option<PackageManager> {
+    let mut node_pm: Option<PackageManager> = None;
+
+    for entry in glob(&(path.to_owned() + "/*")).expect("Failed to read given path.") {
+        match entry
+            .unwrap()
+            .into_os_string()
+            .into_string()
+            .unwrap()
+            .split("/")
+            .last()
+            .unwrap()
+        {
+            "composer.json" => return Some(PackageManager::Composer),
+            "requirements.txt" => return Some(PackageManager::Pip),
+            "package.json" => {
+                if node_pm.is_none() {
+                    node_pm = Some(PackageManager::Npm);
+                }
+            }
+            "yarn.lock" => node_pm = Some(PackageManager::Yarn),
+            "pnpm-lock.yaml" => node_pm = Some(PackageManager::Pnpm),
+            _ => (),
+        }
+    }
+
+    node_pm
+}
+
+#[derive(Debug)]
+struct StackChecker {
+    package_manager: PackageManager,
+}
+
+impl StackChecker {
+    fn new(path: &str) -> StackChecker {
+        let package_manager =
+            detect_package_manager(path).expect("Failed to detect package manager.");
+
+        StackChecker { package_manager }
+    }
+}
 
 fn main() {
     let path = "/workspaces/laravel";
-
-    // Check if direcotry exists
-    if !Path::new(path).exists() {
-        println!("Path does not exist");
-        return;
-    }
-
-    // Read directory
-    let files = fs::read_dir(path).unwrap();
-
-    match StackChecker::check(files, Stack::Laravel) {
-        true => println!("Laravel"),
-        false => println!("Not Laravel"),
-    }
+    let stack_checker = StackChecker::new(path);
 }
 
 enum Stack {
     Laravel,
 }
 
-struct StackChecker {}
+// struct StackChecker {}
 
-impl StackChecker {
-    // Search for file in files
-    fn has_file(files: ReadDir, file_name: &str) -> Option<DirEntry> {
-        for file in files {
-            let current = file.unwrap();
-            let name = current.file_name().into_string().unwrap();
-            if name == file_name {
-                return Some(current);
-            }
-        }
-        None
-    }
+// impl UsesComposerPackageManager for StackChecker {}
 
-    fn has_laravel_package(file: DirEntry) -> bool {
-        let contents = fs::read_to_string(file.path()).unwrap();
-        let re = Regex::new(r#""laravel/framework": "#).unwrap();
-        match re.captures(&contents) {
-            Some(_) => true,
-            None => false,
-        }
-    }
+// impl StackChecker {
+//     fn is_laravel(files: ReadDir) -> bool {
+//         let has_composer = has_file(files, "composer.json");
 
-    fn is_laravel(files: ReadDir) -> bool {
-        let has_composer = Self::has_file(files, "composer.json");
+//         match has_composer {
+//             Some(composer) => match Self::has_laravel_package(composer) {
+//                 true => true,
+//                 false => false,
+//             },
+//             None => false,
+//         }
+//     }
 
-        match has_composer {
-            Some(composer) => match Self::has_laravel_package(composer) {
-                true => true,
-                false => false,
-            },
-            None => false,
-        }
-    }
-
-    fn check(files: ReadDir, stack: Stack) -> bool {
-        match stack {
-            Stack::Laravel => Self::is_laravel(files),
-        }
-    }
-}
+//     fn check(files: ReadDir, stack: Stack) -> bool {
+//         match stack {
+//             Stack::Laravel => Self::is_laravel(files),
+//         }
+//     }
+// }
