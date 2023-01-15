7cbc4751eadc44694bec986c0f377f2ea3cfe624
Mosaab Emam <mosaab.emam123@gmail.com>
2023-01-15 07:42:20 +0000

    Extracting some data, spagetti code

---
+++ src/enms.rs
@@ -0,0 +1,20 @@
+#[derive(Debug)]
+pub enum ProgrammingLanguage {
+    PHP,
+    Python,
+    Javascript,
+}
+
+#[derive(Debug)]
+pub enum PackageManager {
+    Composer,
+    Pip,
+    Npm,
+    Yarn,
+    Pnpm,
+}
+
+#[derive(Debug)]
+pub enum PHPFramework {
+    Laravel,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,173 @@
-use stackcheck::{has_file, PackageManager, UsesComposerPackageManager};
+mod enms;
+
+use enms::{PHPFramework, PackageManager, ProgrammingLanguage};
+use serde_derive::{Deserialize, Serialize};
+use serde_json::Value;
+use std::fs::read_to_string;
 extern crate glob;
 use glob::glob;
-use std::fmt::Display;
-use std::fs::{self, ReadDir};
-use std::path::Path;
 
-fn detect_package_manager(path: &str) -> Option<PackageManager> {
-    let mut node_pm: Option<PackageManager> = None;
+fn main() {
+    let path = "/workspaces/laravel";
+    let pm_tools: PMTools = path.into();
+    let stack_checker = StackChecker::new(pm_tools);
 
-    for entry in glob(&(path.to_owned() + "/*")).expect("Failed to read given path.") {
-        match entry
-            .unwrap()
-            .into_os_string()
-            .into_string()
-            .unwrap()
-            .split("/")
-            .last()
-            .unwrap()
-        {
-            "composer.json" => return Some(PackageManager::Composer),
-            "requirements.txt" => return Some(PackageManager::Pip),
-            "package.json" => {
-                if node_pm.is_none() {
-                    node_pm = Some(PackageManager::Npm);
-                }
-            }
-            "yarn.lock" => node_pm = Some(PackageManager::Yarn),
-            "pnpm-lock.yaml" => node_pm = Some(PackageManager::Pnpm),
-            _ => (),
-        }
-    }
+    println!("{:?}", stack_checker);
+}
 
-    node_pm
+#[derive(Serialize, Deserialize, Debug)]
+struct ComposerJsonFile {
+    name: String,
+    license: String,
+    require: Value,
 }
 
 #[derive(Debug)]
 struct StackChecker {
+    name: String,
+    license: String,
     package_manager: PackageManager,
+    programming_language: ProgrammingLanguage,
+    framework: PHPFramework,
 }
 
 impl StackChecker {
-    fn new(path: &str) -> StackChecker {
-        let package_manager =
-            detect_package_manager(path).expect("Failed to detect package manager.");
-
-        StackChecker { package_manager }
+    pub fn new(pm_tools: PMTools) -> Self {
+        StackChecker {
+            name: pm_tools.get_name(),
+            license: pm_tools.get_license(),
+            package_manager: pm_tools.get_package_manager(),
+            programming_language: pm_tools.get_programming_language(),
+            framework: pm_tools.get_framework().unwrap(),
+        }
     }
 }
 
-fn main() {
-    let path = "/workspaces/laravel";
-    let stack_checker = StackChecker::new(path);
+#[derive(Debug)]
+struct PMTools {
+    package_manager: PackageManager,
+    contents: String,
 }
 
-enum Stack {
-    Laravel,
+impl From<&str> for PMTools {
+    fn from(path: &str) -> Self {
+        let package_manager =
+            Self::detect_package_manager(path).expect("Failed to detect package manager.");
+
+        let contents = match package_manager {
+            PackageManager::Composer => read_to_string(path.to_owned() + "/composer.json").unwrap(),
+            _ => panic!("Package manager not supported yet."),
+        };
+
+        PMTools {
+            contents,
+            package_manager,
+        }
+    }
 }
 
-// struct StackChecker {}
+impl PMTools {
+    pub fn detect_package_manager(path: &str) -> Option<PackageManager> {
+        let mut node_pm: Option<PackageManager> = None;
 
-// impl UsesComposerPackageManager for StackChecker {}
+        for entry in glob(&(path.to_owned() + "/*")).expect("Failed to read given path.") {
+            match entry
+                .unwrap()
+                .into_os_string()
+                .into_string()
+                .unwrap()
+                .split("/")
+                .last()
+                .unwrap()
+            {
+                "composer.json" => return Some(PackageManager::Composer),
+                "requirements.txt" => return Some(PackageManager::Pip),
+                "package.json" => {
+                    if node_pm.is_none() {
+                        node_pm = Some(PackageManager::Npm);
+                    }
+                }
+                "yarn.lock" => node_pm = Some(PackageManager::Yarn),
+                "pnpm-lock.yaml" => node_pm = Some(PackageManager::Pnpm),
+                _ => (),
+            }
+        }
 
-// impl StackChecker {
-//     fn is_laravel(files: ReadDir) -> bool {
-//         let has_composer = has_file(files, "composer.json");
+        node_pm
+    }
 
-//         match has_composer {
-//             Some(composer) => match Self::has_laravel_package(composer) {
-//                 true => true,
-//                 false => false,
-//             },
-//             None => false,
-//         }
-//     }
+    pub fn get_package_manager(&self) -> PackageManager {
+        match self.package_manager {
+            PackageManager::Composer => PackageManager::Composer,
+            PackageManager::Pip => PackageManager::Pip,
+            PackageManager::Npm => PackageManager::Npm,
+            PackageManager::Yarn => PackageManager::Yarn,
+            PackageManager::Pnpm => PackageManager::Pnpm,
+        }
+    }
 
-//     fn check(files: ReadDir, stack: Stack) -> bool {
-//         match stack {
-//             Stack::Laravel => Self::is_laravel(files),
-//         }
-//     }
-// }
+    pub fn get_name(&self) -> String {
+        match self.package_manager {
+            PackageManager::Composer => {
+                let composer_json_file =
+                    serde_json::from_str::<ComposerJsonFile>(&self.contents).unwrap();
+                composer_json_file.name
+            }
+            _ => panic!("Package manager not supported yet."),
+        }
+    }
+
+    pub fn get_license(&self) -> String {
+        match self.package_manager {
+            PackageManager::Composer => {
+                let composer_json_file =
+                    serde_json::from_str::<ComposerJsonFile>(&self.contents).unwrap();
+                composer_json_file.license
+            }
+            _ => panic!("Package manager not supported yet."),
+        }
+    }
+
+    pub fn get_packages(&self) -> Vec<(String, String)> {
+        match self.package_manager {
+            PackageManager::Composer => {
+                let composer_json_file =
+                    serde_json::from_str::<ComposerJsonFile>(&self.contents).unwrap();
+                composer_json_file
+                    .require
+                    .as_object()
+                    .unwrap()
+                    .iter()
+                    .map(|(k, v)| (k.to_string(), v.to_string()))
+                    .collect()
+            }
+            _ => panic!("Package manager not supported yet."),
+        }
+    }
+
+    pub fn get_programming_language(&self) -> ProgrammingLanguage {
+        match self.package_manager {
+            PackageManager::Composer => ProgrammingLanguage::PHP,
+            PackageManager::Pip => ProgrammingLanguage::Python,
+            PackageManager::Npm | PackageManager::Yarn | PackageManager::Pnpm => {
+                ProgrammingLanguage::Javascript
+            }
+        }
+    }
+
+    pub fn get_framework(&self) -> Option<PHPFramework> {
+        if self.is_laravel() {
+            return Some(PHPFramework::Laravel);
+        }
+
+        None
+    }
+
+    pub fn has_package(&self, package: &str) -> bool {
+        self.get_packages().iter().any(|(k, _)| k == package)
+    }
+
+    pub fn is_laravel(&self) -> bool {
+        self.has_package("laravel/framework")
+    }
+}
