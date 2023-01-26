8ce328f1cc6f3d4dc777f333b266c73ae1750622
Mosaab Emam <mosaab.emam123@gmail.com>
2023-01-26 06:33:46 +0000

    :heavy_plus_sign: clap & use it to parse arguments

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,36 @@ mod enms;
 mod pm_tools;
 mod stack_checker;
 
+use core::panic;
+
+use clap::Parser;
+use enms::OutputFormat;
 use pm_tools::PMTools;
 use stack_checker::StackChecker;
 
-fn main() {
-    let path = "/workspaces/laravel";
-    let pm_tools: PMTools = path.into();
-    let stack_checker = StackChecker::new(pm_tools);
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = ".")]
+    path: String,
 
-    stack_checker.output(enms::OutputFormat::YAML);
+    #[arg(short, long, default_value = "stdout")]
+    output_format: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    let output_format: OutputFormat = match args.output_format.as_str() {
+        "stdout" => OutputFormat::STDOUT,
+        "json" => OutputFormat::JSON,
+        "yml" => OutputFormat::YAML,
+        "yaml" => OutputFormat::YAML,
+        "html" => OutputFormat::HTML,
+        "pdf" => OutputFormat::PDF,
+        _ => panic!("Output format not supported yet."),
+    };
+
+    let pm_tools: PMTools = args.path.into();
+    let stack_checker = StackChecker::new(pm_tools);
+    stack_checker.output(output_format);
 }
